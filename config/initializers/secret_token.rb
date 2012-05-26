2fbd21421419579ff61cb4802bd06e8e22da4989
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2012-05-26 12:36:38 +0900

    rails new book_memo2 -T
    memo:
    rvm use 1.9.3
    rvm gemset create book_memo2
    rvm use @book_memo2
    gem i bundler
    gem i rails
    rails new book_memo2 -T
    cd book_memo2
    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookMemo2::Application.config.secret_token = '97ab978246c99f550f2228ffb4b6b7bd6d1f515ec3810c3d1d7225f631f7d31defa4f7bc6cd11d9376e19711a951135b188bff86f6a3bc646aa95fc54ba87bb7'
