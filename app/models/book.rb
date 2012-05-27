80aef372f7f6b22ec3955509a4722cfbb750d341
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2012-05-28 02:19:08 +0900

    Add Book#total_books_count

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
+# encoding: UTF-8
 class Book < ActiveRecord::Base
   attr_accessible :memo, :purchased_on, :title
   validates :title, :presence => true
+
+  before_create :total_books_count
+
+  def total_books_count
+    self.memo += "【 累計冊数#{Book.count + 1} 】"
+  end
 end
