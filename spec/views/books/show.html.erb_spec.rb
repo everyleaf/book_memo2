af2a30c7c9b84319bcc7f3b7545c75d37dbade69
takatsugu.ishioka <takatsugu.ishioka@mail.rakuten.com>
2012-06-15 23:16:16 +0900

    show.html.rbのspec修正

--- spec/views/books/show.html.erb_spec.rb
+++ spec/views/books/show.html.erb_spec.rb
@@ -6,12 +6,13 @@ describe "books/show" do
       :title => "Title",
       :memo => "MyText"
     ))
+    @memo = Memo.new
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Title/)
-    rendered.should match(/MyText/)
+     rendered.should match(/Title/)
+     rendered.should match(/MyText/)
   end
 end
