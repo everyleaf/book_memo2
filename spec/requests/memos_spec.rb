5b6fa7153cd4e24bbc7a46f13ce9782c27aabc86
Kenichiro Yasuda <kenichiro.yasuda@mail.rakuten.com>
2012-06-15 01:20:39 +0000

    Book詳細画面からメモを二つ登録するリクエストスペックを追加

--- spec/requests/memos_spec.rb
+++ spec/requests/memos_spec.rb
@@ -23,6 +23,26 @@ describe "Memos" do
     end
   end
   
+  describe "PUT /memos 2 times" do
+    let!(:book){ FactoryGirl.create :book }
+    subject { page }
+    
+    let(:memo_body) { ["memo body", "memo body 2"] }
+    
+    before do
+      visit "/books/#{book.id}"
+      fill_in "memo[body]", with: memo_body[0]
+      click_on "Create Memo"
+      fill_in "memo[body]", with: memo_body[1]
+      click_on "Create Memo"
+    end
+    
+    it "メモが追加されている" do
+      should have_content(memo_body[0])
+      should have_content(memo_body[1])
+    end
+  end
+  
   describe "DELETE /memos/:id" do
     let!(:book) { FactoryGirl.create :book }
     let(:memo_body) { "memo body" }
