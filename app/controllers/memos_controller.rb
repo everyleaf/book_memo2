a777c9991fc53bc086bcd2502e556bf079c0208f
Takeshi Takizawa <TakiTake.create@gmail.com>
2012-06-15 06:18:00 +0000

    enable creating memo

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,9 +1,24 @@
 class MemosController < ApplicationController
+  before_filter :find_book
+
   def new
-    @book = Book.find(params[:book_id])
     @memo = @book.memos.new
   end
 
+  def create
+    @memo = @book.memos.new(params[:memo])
+    if @memo.save
+      redirect_to book_path(@book), :notice => 'saved memo.'
+    else
+      render :new
+    end
+  end
+
   def delete
+  end
+
+  private
+  def find_book
+    @book = Book.find(params[:book_id])
   end
 end
--- spec/controllers/memos_controller_spec.rb
+++ spec/controllers/memos_controller_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe MemosController do
-  def valid_attributes
+  def book_attributes
     { title: 'book title', memo: '' }
+  end
+
+  def valid_attributes(book_id)
+    { book_id: book_id, body: 'memo body' }
   end
 
   describe "GET 'new'" do
     it "returns http success" do
-      book = Book.create! valid_attributes
+      book = Book.create! book_attributes
       get :new, {:book_id => book.to_param}
       response.should be_success
     end
   end
 
-  describe "GET 'delete'" do
+  describe "POST 'create'" do
     it "returns http success" do
-      get 'delete'
+      book = Book.create! book_attributes
+      memo = Memo.create! valid_attributes(book.to_param)
+      post :new, {:book_id => memo.id, :body => memo.body}
       response.should be_success
     end
   end
